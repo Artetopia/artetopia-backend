42299a222ed53374ec1c3f165fc42567d0be9ec2
Víctor Cosme <victorcosmema@gmail.com>
2023-12-15 21:06:33 -0600

    Add item schema

---
+++ src/models/item.model.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+const itemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 40,
+    maxLength: 160,
+  },
+  category: {},
+  inventory: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  images: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'multimedia',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  craftsman: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'craftsman',
+  },
+});
+
+module.exports = mongoose.model('item', itemSchema);
