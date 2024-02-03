72737443a5f1f1eda66209cf1dc6249a712e6173
David Krumholz <davidkrumholz@yahoo.com.mx>
2024-02-03 15:40:11 -0600

    Merge feature/landing-categories into develop

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const AuthRouter = require("./routes/auth.router");
 const CraftmanRouter = require("./routes/craftsmen.router");
+const TemplateColorsRouter = require("./routes/templateColors.router");
 const CategoryRouter = require("./routes/category.router");
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ limit: "50mb" }));
 
 app.use("/auth", AuthRouter);
 app.use("/craftman", CraftmanRouter);
+app.use("/templateColors", TemplateColorsRouter);
 app.use("/categories", CategoryRouter);
 
 app.get("/", (request, response) => {
