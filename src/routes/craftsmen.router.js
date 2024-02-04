ae096dd99e07c979e92c70ee46ec87072335e4cf
Víctor Cosme <victorcosmema@gmail.com>
2024-02-04 03:25:01 +0100

    Add get all craftsmen

--- src/routes/craftsmen.router.js
+++ src/routes/craftsmen.router.js
@@ -5,117 +5,141 @@ const auth = require("../middleware/auth.middleware");
 const router = express.Router();
 
 router.post("/product", auth, async (request, response) => {
-    try {
-        const productCreated = await CraftmanUseCase.createProduct(request.user, request.body);
-        response.json({
-            message: "Producto creado",
-            data: {
-                product: productCreated
-            }
-        })
-    } catch (error) {
-        response.status(error.status || 500);
-        response.json({
-            message: "Algo salio mal",
-            error: error.message,
-        });
-    }
+  try {
+    const productCreated = await CraftmanUseCase.createProduct(
+      request.user,
+      request.body
+    );
+    response.json({
+      message: "Producto creado",
+      data: {
+        product: productCreated,
+      },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      message: "Algo salio mal",
+      error: error.message,
+    });
+  }
 });
 
 router.get("/productByCraftman", auth, async (request, response) => {
-    try {
-        const products = await CraftmanUseCase.getAllProductsByCraftman(request.user);
-        response.json({
-            message: "productos",
-            data: {
-                products
-            }
-        })
-    } catch (error) {
-        response.status(error.status || 500);
-        response.json({
-            message: "Algo salio mal",
-            error: error.message,
-        });
-    }
+  try {
+    const products = await CraftmanUseCase.getAllProductsByCraftman(
+      request.user
+    );
+    response.json({
+      message: "productos",
+      data: {
+        products,
+      },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      message: "Algo salio mal",
+      error: error.message,
+    });
+  }
 });
 
 router.patch("/product/:productId", auth, async (request, response) => {
-    try {
-        const productId = request.params.productId;
-        const updatedProduct = await CraftmanUseCase.updateProduct(productId, request.body);
-        response.json({
-            message: "Se actualizo el producto",
-            data: {
-                product: updatedProduct
-            }
-        });
-        
-    } catch(error) {
-        response.status(error.status || 500);
-        response.json({
-            message: "Algo salio mal",
-            error: error.message,
-        });
-    }
+  try {
+    const productId = request.params.productId;
+    const updatedProduct = await CraftmanUseCase.updateProduct(
+      productId,
+      request.body
+    );
+    response.json({
+      message: "Se actualizo el producto",
+      data: {
+        product: updatedProduct,
+      },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      message: "Algo salio mal",
+      error: error.message,
+    });
+  }
 });
 
 router.delete("/product/:productId", auth, async (request, response) => {
-    try {
-        const productId = request.params.productId;
-        const productDeleted = await CraftmanUseCase.deleteProduct(productId);
-        response.json({
-            message: "Producto eliminado",
-            data: {
-                product: productDeleted
-            }
-        });
-    } catch (error) {
-        response.status(error.status || 500);
-        response.json({
-            message: "Algo salio mal",
-            error: error.message,
-        });
-    }
+  try {
+    const productId = request.params.productId;
+    const productDeleted = await CraftmanUseCase.deleteProduct(productId);
+    response.json({
+      message: "Producto eliminado",
+      data: {
+        product: productDeleted,
+      },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      message: "Algo salio mal",
+      error: error.message,
+    });
+  }
 });
 
 router.get("/bankAccount", auth, async (request, response) => {
-    try {
-        const bankAccount = await CraftmanUseCase.getBankInformation(request.user);
-        response.json({
-            message: "Cuenta de banco creada",
-            data: {
-                bankAccount: bankAccount
-            }
-        });
-    } catch (error) {
-        response.status(error.status || 500);
-        response.json({
-            message: "Algo salio mal",
-            error: error.message,
-        });
-    }
+  try {
+    const bankAccount = await CraftmanUseCase.getBankInformation(request.user);
+    response.json({
+      message: "Cuenta de banco creada",
+      data: {
+        bankAccount: bankAccount,
+      },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      message: "Algo salio mal",
+      error: error.message,
+    });
+  }
 });
 
-router.patch("/registrationProgress/:step", auth, async(request, response) => {
-    try {
-        const step = request.params.step;
-        const stepResult = await CraftmanUseCase.setProgressCraftman(request.user, step);
-        response.json({
-            message: "Se asigno correctamente el progreso",
-            data: {
-                craftman: stepResult
-            }
-        });
+router.patch("/registrationProgress/:step", auth, async (request, response) => {
+  try {
+    const step = request.params.step;
+    const stepResult = await CraftmanUseCase.setProgressCraftman(
+      request.user,
+      step
+    );
+    response.json({
+      message: "Se asigno correctamente el progreso",
+      data: {
+        craftman: stepResult,
+      },
+    });
+  } catch (error) {
+    response.status(error.status || 500);
+    response.json({
+      message: "Algo salio mal",
+      error: error.message,
+    });
+  }
+});
 
-    } catch(error) {
-        response.status(error.status || 500);
-        response.json({
-            message: "Algo salio mal",
-            error: error.message,
-        });
-    }
-})
-
+router.get("/allCraftsmen", async (request, response) => {
+  try {
+    const allCraftsmen = await CraftmanUseCase.getAllCraftsmen();
+    response.json({
+      message: "Obtener todos los artesanos de la BBDD",
+      data: { craftsmen: allCraftsmen },
+    });
+  } catch (error) {
+    response.status(500);
+    response.json({
+      message: "Algo fue mal",
+      error: error.message,
+    });
+  }
+});
 
 module.exports = router;
--- src/useCases/craftsman.usecase.js
+++ src/useCases/craftsman.usecase.js
@@ -88,23 +88,31 @@ async function updateProduct(productId, productObject) {
     return image.toString();
   });
 
-const uniqueValues = ObjectIdImagesString.filter((value) => !productObject.images.includes(value));
+  const uniqueValues = ObjectIdImagesString.filter(
+    (value) => !productObject.images.includes(value)
+  );
 
-  if(uniqueValues.length > 0) {
-    const images = await Promise.all(uniqueValues.map( async (image) => {
+  if (uniqueValues.length > 0) {
+    const images = await Promise.all(
+      uniqueValues.map(async (image) => {
         const objectIdMultimedia = new mongoose.Types.ObjectId(image);
         const multimedia = await Multimedia.findById(objectIdMultimedia);
         const deletedFile = await s3Service.s3DeleteFile(multimedia);
-        if(deletedFile) {
-            const productUpdated = await Product.findByIdAndUpdate(productId, {$pull: {images: multimedia._id}});
-            if(productUpdated) {
-                const deleteMultimedia = await Multimedia.findByIdAndDelete(multimedia._id);
-                if(!deleteMultimedia) {
-                    throw new createError(400, "Error al eliminar la imagen");
-                }
+        if (deletedFile) {
+          const productUpdated = await Product.findByIdAndUpdate(productId, {
+            $pull: { images: multimedia._id },
+          });
+          if (productUpdated) {
+            const deleteMultimedia = await Multimedia.findByIdAndDelete(
+              multimedia._id
+            );
+            if (!deleteMultimedia) {
+              throw new createError(400, "Error al eliminar la imagen");
             }
+          }
         }
-      }));
+      })
+    );
   }
 
   //subir todas las imagenes que hacen falta por subir a S3 y asginarlas a multimedia y al producto
@@ -136,59 +144,71 @@ const uniqueValues = ObjectIdImagesString.filter((value) => !productObject.image
       return saveImage._id;
     })
   );
-  
+
   //Actualizar producto
-  
+
   productObject.images = resultsSaveImages;
 
-  const productUpdated = await Product.findByIdAndUpdate(productId, {$push: {images: productObject.images}}, {new: true}).populate({path: "images", select: "url key"});
+  const productUpdated = await Product.findByIdAndUpdate(
+    productId,
+    { $push: { images: productObject.images } },
+    { new: true }
+  ).populate({ path: "images", select: "url key" });
 
-  if(!productUpdated) {
-    throw new createError(400, "Error al actualizar el producto")
+  if (!productUpdated) {
+    throw new createError(400, "Error al actualizar el producto");
   }
 
   return productUpdated;
 }
 
 async function deleteProduct(productId) {
-
   console.log("productId", productId);
-  if(!mongoose.isValidObjectId(productId)) {
+  if (!mongoose.isValidObjectId(productId)) {
     throw new createError(400, "Id invalido");
   }
 
   const productExist = await Product.findById(productId);
 
-  if(!productExist) {
+  if (!productExist) {
     throw new createError(404, "El producto no existe");
   }
 
-  const images = await Promise.all(productExist.images.map(async (image) => {
-    const objectIdMultimedia = new mongoose.Types.ObjectId(image);
-    const multimedia = await Multimedia.findById(objectIdMultimedia);
-    if(!multimedia) {
-      throw new createError(400, "No se encontro una imagen");
-    }
-    const deletedFile = await s3Service.s3DeleteFile(multimedia);
-    if(deletedFile) {
-      const productUpdated = await Product.findByIdAndUpdate(productExist._id, {$pull: {images: multimedia._id}});
-      if(productUpdated) {
-        const deleteMultimedia = await Multimedia.findByIdAndDelete(multimedia._id);
-        if(!deleteMultimedia) {
-          throw new createError(400, "Error al eliminar la imagen");
+  const images = await Promise.all(
+    productExist.images.map(async (image) => {
+      const objectIdMultimedia = new mongoose.Types.ObjectId(image);
+      const multimedia = await Multimedia.findById(objectIdMultimedia);
+      if (!multimedia) {
+        throw new createError(400, "No se encontro una imagen");
+      }
+      const deletedFile = await s3Service.s3DeleteFile(multimedia);
+      if (deletedFile) {
+        const productUpdated = await Product.findByIdAndUpdate(
+          productExist._id,
+          { $pull: { images: multimedia._id } }
+        );
+        if (productUpdated) {
+          const deleteMultimedia = await Multimedia.findByIdAndDelete(
+            multimedia._id
+          );
+          if (!deleteMultimedia) {
+            throw new createError(400, "Error al eliminar la imagen");
+          }
         }
       }
-    }
-  }));
+    })
+  );
 
   const product = await Product.findById(productId);
-  if(product.images.length > 0) {
-    throw new createError(400, "El producto no se puede eliminar, ya que cuenta imagenes existentes");
+  if (product.images.length > 0) {
+    throw new createError(
+      400,
+      "El producto no se puede eliminar, ya que cuenta imagenes existentes"
+    );
   }
 
   constProductDeleted = await Product.findByIdAndDelete(productId);
   return constProductDeleted;
-  
 }
 
 async function getAllProductsByCraftman(userId) {
@@ -202,7 +222,9 @@ async function getAllProductsByCraftman(userId) {
     throw new createError(404, "Craftman no encontrado");
   }
 
-  const products = await Product.find({craftsman: new mongoose.Types.ObjectId(getCraftman._id)}).populate({path: 'images', select: "url key"});
+  const products = await Product.find({
+    craftsman: new mongoose.Types.ObjectId(getCraftman._id),
+  }).populate({ path: "images", select: "url key" });
 
   return products;
 }
@@ -216,17 +238,17 @@ async function getBankInformation(userId) {
 
   const user = await User.findById(craftmanObject);
 
-  if(!user) {
+  if (!user) {
     throw new createError(404, "Usuario no encontrado");
   }
 
-  const craftman = await Craftman.findOne({user: craftmanObject});
+  const craftman = await Craftman.findOne({ user: craftmanObject });
 
-  if(!craftman) {
+  if (!craftman) {
     throw new createError(404, "Craftman no encontrado");
   }
 
-  if(!craftman.accountIdStripe) {
+  if (!craftman.accountIdStripe) {
     const accountService = await stripe.accounts.create({
       type: "express",
       country: "MX",
@@ -241,42 +263,48 @@ async function getBankInformation(userId) {
       },
     });
 
-    const craftmanUpdated = await Craftman.findByIdAndUpdate(craftman._id, {accountIdStripe: accountService.id});
-    if(!craftmanUpdated) {
+    const craftmanUpdated = await Craftman.findByIdAndUpdate(craftman._id, {
+      accountIdStripe: accountService.id,
+    });
+    if (!craftmanUpdated) {
       throw new createError(400, "Error al actualizar el artesano");
     }
 
     const accountLink = await stripe.accountLinks.create({
       account: accountService.id,
-      refresh_url: 'http://localhost:5173/',
-      return_url: 'http://localhost:5173/',
-      type: 'account_onboarding',
+      refresh_url: "http://localhost:5173/",
+      return_url: "http://localhost:5173/",
+      type: "account_onboarding",
     });
 
     return {
       object: accountLink.object,
-      url: accountLink.url
-    }
+      url: accountLink.url,
+    };
   } else {
-    const accountService = await stripe.accounts.retrieve(craftman.accountIdStripe);
-    if(accountService.details_submitted) {
-      const loginLink = await stripe.accounts.createLoginLink(craftman.accountIdStripe);
+    const accountService = await stripe.accounts.retrieve(
+      craftman.accountIdStripe
+    );
+    if (accountService.details_submitted) {
+      const loginLink = await stripe.accounts.createLoginLink(
+        craftman.accountIdStripe
+      );
       return {
         object: loginLink.object,
-        url: loginLink.url
-      }
+        url: loginLink.url,
+      };
     } else {
       const accountLink = await stripe.accountLinks.create({
         account: craftman.accountIdStripe,
-        refresh_url: 'http://localhost:5173/',
-        return_url: 'http://localhost:5173/',
-        type: 'account_onboarding',
+        refresh_url: "http://localhost:5173/",
+        return_url: "http://localhost:5173/",
+        type: "account_onboarding",
       });
 
       return {
         object: accountLink.object,
-        url: accountLink.url
-      }
+        url: accountLink.url,
+      };
     }
   }
 }
@@ -290,26 +318,34 @@ async function setProgressCraftman(userId, step) {
 
   const user = await User.findById(craftmanObject);
 
-  if(!user) {
+  if (!user) {
     throw new createError(404, "Usuario no encontrado");
   }
 
-  const craftman = await Craftman.findOne({user: craftmanObject});
+  const craftman = await Craftman.findOne({ user: craftmanObject });
 
-  if(!craftman) {
+  if (!craftman) {
     throw new createError(404, "Craftman no encontrado");
   }
 
-  if(step > 8) {
+  if (step > 8) {
     throw new createError(400, "Error, porfavor contactar al administrador");
   }
 
-  if(craftman.step <= step) {
-    const craftmanUpdated = await Craftman.findByIdAndUpdate(craftman._id, {step}, { new: true });
+  if (craftman.step <= step) {
+    const craftmanUpdated = await Craftman.findByIdAndUpdate(
+      craftman._id,
+      { step },
+      { new: true }
+    );
     return craftmanUpdated;
   } else {
     return craftman;
   }
+}
+
+async function getAllCraftsmen() {
+  return await Craftman.find();
 }
 
 module.exports = {
@@ -318,5 +354,6 @@ module.exports = {
   updateProduct,
   deleteProduct,
   getBankInformation,
-  setProgressCraftman
+  setProgressCraftman,
+  getAllCraftsmen,
 };
